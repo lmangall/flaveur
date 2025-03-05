f23ffdda32d5776d738a63ec844a1e563a0b6d62
lmangall <l.mangallon@gmail.com>
2025-03-05 18:47:14 +0100

    feat: retrieve user info(id and name)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { SignIn } from "@clerk/nextjs";
+import { SignIn, useUser } from "@clerk/nextjs";
 
 export default function Home() {
+  const { user, isSignedIn } = useUser();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <SignIn />
-        <div className="flex gap-4 items-center flex-col sm:flex-row"></div>
-        <div>
-          <Button>Click me</Button>
-        </div>
+        {!isSignedIn ? (
+          <SignIn routing="hash" />
+        ) : (
+          <div>
+            <h1>Welcome, {user?.fullName}</h1>
+            <p>Your User ID: {user?.id}</p>
+            <Button>Click me</Button>
+          </div>
+        )}
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         footer
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,13 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
+//https://www.youtube.com/watch?v=vi9VhhMFpWI
+//check public routes for clerk to access session but not protect
+
 export const config = {
   matcher: [
     // Allow the sign-in route to be accessible
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)|api|trpc).*)",
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)|api|trpc|[[...rest]]).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
   ],
