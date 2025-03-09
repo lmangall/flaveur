bf24260aeea5af1d5ed651697c783cd502a057a8
lmangall <l.mangallon@gmail.com>
2025-03-09 14:25:15 +0100

    Fix(build): add Clerk routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,21 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+// defining a set of public routes and allowing access to them without authentication
+export default clerkMiddleware((auth, req) => {
+  const publicRoutes = ["", "/", "/sign-in", "/sign-up"];
+  const isPublicRoute = publicRoutes.includes(new URL(req.url).pathname);
+
+  if (isPublicRoute) {
+    return;
+  }
+});
 
 //https://www.youtube.com/watch?v=vi9VhhMFpWI
 //check public routes for clerk to access session but not protect
 
 export const config = {
   matcher: [
-    // Allow the sign-in route to be accessible
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)|api|trpc|[[...rest]]).*)",
-    // Always run for API routes
+    "/((?!_next|[^?]*\\.(?:html?|css|js|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)|api|trpc|[[...rest]]).*)",
     "/(api|trpc)(.*)",
   ],
 };
